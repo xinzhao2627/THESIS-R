2e1e9a5ae88b769517f24cf035898e6dd245b62a
Rainnand Montaniel <94624435+xinzhao2627@users.noreply.github.com>
2025-09-11 21:55:52 +0800

    Update ImageModel.java documentation

--- mobile/app/src/main/java/com/example/explicitapp3/ImageModel.java
+++ mobile/app/src/main/java/com/example/explicitapp3/ImageModel.java
@@ -1,4 +1,35 @@
 package com.example.explicitapp3;
+/**
+IMAGE MODEL DOCUMENTATION
+<p> This is the image function for the overlay foreground
+Example
+{@code
+    ImageModel imageModel = new ImageModel();
+    imageModel.initModel();
+    imageModel.destroy();
+}
 
+@author xinzhao2627 (R. Montaniel)
+@see MainActivity#clasa
+*/
 public class ImageModel {
+   Interpreter interpreter;
+
+   /** TODO: 
+   * initilizeModel
+   * @param: String path
+   * @param: Context mcontext
+     @impliednote: tflite types are isolated, check assets if include metadata
+   */ 
+   public void initModel(){}
+
+   public void pauseState(boolean _state)     {
+      /** 
+        tflite states are mutable
+        TODO:
+          - 
+      */
+}
+   public void destroy(){}
+
 }
